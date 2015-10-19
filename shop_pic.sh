a09d22a1c64e75364f92d97268b32cde12fbe3f2
Timm Murray <tmurray@wumpus-cave.net>
2015-10-19 17:36:00 -0500

    Flesh out script for taking picture during open shop

--- shop_pic.sh
+++ shop_pic.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 source shop_pic_conf.sh
 
+PREV_OPEN_STATUS=0
+
 while true; do
-    IS_OPEN=`curl http://${SERVER}/shop_open`
+    IS_OPEN=`curl -k https://${SERVER}/shop_open`
+    echo "Shop open status: ${IS_OPEN}"
     
-    if [ ${IS_OPEN} == 0 ]; do 
-        echo "Sending pic . . . "
+    if [ "${IS_OPEN}" -eq 1 ]
+    then
+        echo "Taking pic . . . "
         raspistill -n -t 1 \
             -w ${WIDTH} -h ${HEIGHT} -q ${JPEG_QUALITY} \
             -o ${OUT_FILE}
+        echo "Sending pic . . . "
         scp -i ${PRIVATE_KEY_FILE} \
             ${OUT_FILE} \
             ${SERVER_USERNAME}@${SERVER_HOST}:${SERVER_UPLOAD_PATH}
-    done
+    else
+        if [ "${PREV_OPEN_STATUS}" -eq 1 ]
+        then
+            echo "Sending default pic . . ."
+            scp -i ${PRIVATE_KEY_FILE} \
+                ${DEFAULT_PIC} \
+                ${SERVER_USERNAME}@${SERVER_HOST}:${SERVER_UPLOAD_PATH}
+        else
+            echo "Doing nothing . . ."
+        fi
+    fi
 
+    PREV_OPEN_STATUS=${IS_OPEN}
     sleep ${SLEEP_TIME}
 done
